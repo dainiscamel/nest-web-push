2a9139e8069c924b0e0237541a97313c1a1c10d7
dikang <dikang@deepnoid.com>
2025-05-21 11:07:24 +0900

    feat: 알림설정관련 컨트롤러 모듈 서비스로직 dto 추가

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,55 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 import { AppService } from './app.service';
+import {
+  MultipleDeviceNotificationDto,
+  TopicNotificationDto,
+} from './dto/notification.dto';
 
+// endpoint
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('send-notification')
+  @ApiOperation({ summary: 'Send a push notification to a single device' })
+  @ApiResponse({ status: 200, description: 'Notification sent successfully' })
+  async sendNotification(
+    @Body() body: { token: string; title: string; body: string; icon: string },
+  ) {
+    return this.appService.sendNotification({
+      token: body.token,
+      title: body.title,
+      body: body.body,
+      icon: body.icon,
+    });
+  }
+
+  @Post('send-multiple-notifications')
+  @ApiOperation({ summary: 'Send push notifications to multiple devices' })
+  @ApiResponse({ status: 200, description: 'Notifications sent successfully' })
+  async sendMultipleNotifications(@Body() body: MultipleDeviceNotificationDto) {
+    return this.appService.sendNotificationToMultipleTokens({
+      tokens: body.tokens,
+      title: body.title,
+      body: body.body,
+      icon: body.icon,
+    });
+  }
+
+  @Post('send-topic-notification')
+  @ApiOperation({ summary: 'Send a push notification to a topic' })
+  @ApiResponse({
+    status: 200,
+    description: 'Topic notification sent successfully',
+  })
+  async sendTopicNotification(@Body() body: TopicNotificationDto) {
+    return this.appService.sendTopicNotification({
+      topic: body.topic,
+      title: body.title,
+      body: body.body,
+      icon: body.icon,
+    });
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import * as admin from 'firebase-admin';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -7,4 +9,14 @@ import { AppService } from './app.service';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    admin.initializeApp({
+      credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      }),
+    });
+  }
+}
